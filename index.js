a18da1afa18c4b0d8d704cf5775f3bf421d5be15
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-06-06 22:09:47 -0500

    environment variables

--- index.js
+++ index.js
@@ -2,18 +2,25 @@
 
 "use strict";
 
+require("dotenv").config();
+
 const minimist = require("minimist");
+const promptly = require("promptly");
 const { createDb } = require("./lib");
 const argv = minimist(process.argv.slice(2));
 
+const promptPassword = () =>
+  promptly.password("Enter your password: ", { replace: "*" });
+
 async function main() {
-  const db = await createDb("postgres");
+  const db = await createDb(process.env.DB_TYPE);
   const command = argv._.shift();
 
   switch (command) {
     case "users:create":
       try {
-        const { user, pass } = argv;
+        const { user } = argv;
+        const pass = await promptPassword();
         await db.createUser(user, pass);
         console.log(`${user} created`);
       } catch (err) {
@@ -34,6 +41,8 @@ async function main() {
     case "secrets:create":
       try {
         const { user, name, value } = argv;
+        const pass = await promptPassword();
+
         await db.createSecret(user, name, value);
         console.log(`secret: ${name} created`);
       } catch (err) {
@@ -43,7 +52,9 @@ async function main() {
     case "secrets:list":
       try {
         const { user } = argv;
+        const pass = await promptPassword();
         const secrets = await db.listSecrets(user);
+
         secrets.forEach((s) => {
           console.log(`- ${s.name}`);
         });
@@ -54,7 +65,9 @@ async function main() {
     case "secrets:get":
       try {
         const { user, name } = argv;
+        const pass = await promptPassword();
         const secret = await db.getSecret(user, name);
+
         if (!secret) return console.log(`secret ${name} not found`);
         console.log(`- ${secret.name} = ${secret.value}`);
       } catch (err) {
@@ -64,6 +77,8 @@ async function main() {
     case "secrets:update":
       try {
         const { user, name, value } = argv;
+        const pass = await promptPassword();
+
         await db.updateSecret(user, name, value);
         console.log(`secret ${name} updated`);
       } catch (err) {
@@ -73,6 +88,8 @@ async function main() {
     case "secrets:delete":
       try {
         const { user, name } = argv;
+        const pass = await promptPassword();
+
         await db.deleteSecret(user, name);
         console.log(`secret ${name} deleted`);
       } catch (err) {
